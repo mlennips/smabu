67c9eddd6ed6cfcd709531d39a79f50871e263b9
Matthias Lennips <matthiaslennips@hotmail.com>
2024-11-24 11:07:11 +0100

    Sort annual financial statements by fiscal year; Update header rendering in details view; Fix default currency in invoice creation

--- src/lit.smabu.react/src/pages/invoices/InvoiceCreate.tsx
+++ src/lit.smabu.react/src/pages/invoices/InvoiceCreate.tsx
@@ -14,6 +14,7 @@ import { handleAsyncTask } from '../../utils/handleAsyncTask';
 import SelectField from '../../components/controls/SelectField';
 
 const defaultCurrency: Currency = {
+    value: 'EUR',
     isoCode: 'EUR',
     name: 'Euro',
     sign: '€'
