8eea0257da9e8907a51c9a135165b3713cfa297b
Matthias Lennips <matthiaslennips@hotmail.com>
2025-02-23 12:50:28 +0100

    Update react to v19

--- src/LIT.Smabu.API/Program.cs
+++ src/LIT.Smabu.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost",
         builder =>
         {
-            builder.WithOrigins("http://localhost", "http://localhost:5174", "http://localhost:*")
+            builder.WithOrigins("http://localhost", "http://localhost:5173", "http://localhost:*")
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
